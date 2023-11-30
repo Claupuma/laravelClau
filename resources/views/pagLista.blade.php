7784443be37c9038ccf6e058ae49ded38f48d75d
ClaudiaPuma <claudia.pticona@gmail.com>
2023-11-30 10:03:47 -0500

    versionactualizacion

--- resources/views/pagLista.blade.php
+++ resources/views/pagLista.blade.php
@@ -1,11 +1,14 @@
 @extends('pagPlantilla')
 
 @section('titulo')
-    <h1>Lista</h1>
+    <h1>PÁGINA LISTA</h1>
 
 @endsection
 
 @section('seccion')
-    <h3>este codigo de la foto. 7</h3>
+    <h3>LISTA</h3>
+    @foreach($xAlumnos as $item)
+        <p> {{ $item->id}} {{ $item->nomEst }} </p>
+    @endforeach
 
 @endsection
